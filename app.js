b7009e663e2f3f875c2c436b87292bcddb675095
polyhistor2050 <chrisspaul2050@gmail.com>
2023-05-07 22:05:39 +0300

    add API endpoint to generate random images from different categories

--- app.js
+++ app.js
@@ -7,8 +7,7 @@ app.use(express.static("public"));
 
 app.get("/", function(require, respond){
     respond.sendFile(__dirname + "/rate/index.html");
-});
-
+}); 
 
 app.listen(3000, function(){
     console.log("Server is running on port 3000");
--- public/script/script.js
+++ public/script/script.js
@@ -1,3 +1,36 @@
+function updateRandomImage(category) {
+  var data = 'https://api-ninjas.com';
+  $.ajax({
+    method: 'GET',
+    url: 'https://api.api-ninjas.com/v1/randomimage?category=' + category,
+    headers: { 'X-Api-Key': 'aNHSPNm/gccEFOM57PlfFA==WqSeQDC77DBYZh2i', 'Accept': 'image/jpg'},
+    responseType: 'arraybuffer', // Set the response type to arraybuffer to get binary data
+    success: function(result) {
+      // Check if the response contains a valid image binary data
+      if (result && result.image) {
+        const imageUrl = convertBinaryToJpgUrl(result.image);
+        // Update the src attribute of the left-picture and right-picture classes
+        $('.left-picture').attr('src', imageUrl);
+        $('.right-picture').attr('src', imageUrl);
+      } else {
+        console.error('Error: Invalid image data');
+      }
+    },
+    error: function ajaxError(jqXHR) {
+      console.error('Error: ', jqXHR.responseText);
+    }
+  });
+}
+
+function convertBinaryToJpgUrl(binaryData) {
+  const blob = new Blob([binaryData], { type: 'image/jpeg' });
+  const url = URL.createObjectURL(blob);
+  return url;
+}
+ 
+ // Call the function to update the images with a random image in the "nature" category
+ updateRandomImage('city');
+
 function createStarRatings(leftStarsSelector, rightStarsSelector) {
     const leftStars = document.querySelectorAll(leftStarsSelector);
     const rightStars = document.querySelectorAll(rightStarsSelector);
@@ -19,4 +52,6 @@ function createStarRatings(leftStarsSelector, rightStarsSelector) {
   
   // Example usage: create star ratings for elements with class "leftStar" and "rightStar"
   createStarRatings(".leftStar", ".rightStar");
+
   
+ 
